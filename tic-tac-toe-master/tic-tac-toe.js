9bdf6f10f4cc1df28c8b95ac3198a09c911957bc
Ahmalapala <ahmalchambers27@gmail.com>
2022-10-13 22:54:03 -0500

    hover implemented

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -2,6 +2,14 @@ let x = true;
 window.onload=function(){
     var square = document.getElementById("board").children;
     for (let y=0; y < square.length; y++){
+        square[y].setAttribute("class","hover");
+        square[y].addEventListener("mouseover", function(){
+            square[y].classList.add("hover")
+        });
+        square[y].setAttribute("class","hover");
+        square[y].addEventListener("mouseout", function(){
+            square[y].classList.remove("hover")
+        });
         square[y].setAttribute("class","square");
         square[y].addEventListener("click", function(){
             if(x==true){
